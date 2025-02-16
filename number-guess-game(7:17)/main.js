4957eee4fcc8cdf3487803a3ecbb390d18025620
holim chol <ghfla225@gmail.com>
2025-02-17 04:08:07 +0900

    강의 최종

--- number-guess-game(7:17)/main.js
+++ number-guess-game(7:17)/main.js
@@ -2,28 +2,65 @@
  let playButton = document.getElementById("play-button");
  let userInput = document.getElementById("user-input");
  let resultArea = document.getElementById("result-area");
- let correctAnswer = document.getElementById("Correct answer");
+ let correctAnswer = document.getElementById("Correct-answer");
+ let resetButton = document.getElementById("reset-button");
+ let chances =  3;
+ let gameOver = false;
+ let chanceArea = document.getElementById("chance-area");
+ let history = [];
 
  playButton.addEventListener("click", play);
+ resetButton.addEventListener("click",reset);
+ userValue.addEventListener("focus",function(){userValue=""});
 
  function pickRandomNum() {
     computerNum = Math.floor(Math.random()*100)+1;
     console.log("정답 :",computerNum);
-    correctAnswer.textContent = `정답은 ${computerNum}입니다!`;
+    correctAnswer.textContent = `정답은: ${computerNum}입니다!`;
  }
 
  function play() {
     let userValue = userInput.value;
 
+    if(userValue<1 || userValue>100) {
+        resultArea.textContent = "☠️1과 100사이의 숫자를 입력해주세요☠️";
+        return;
+    }
+    if(history.includes(userValue)) {
+        resultArea.textContent = "이미 입력한 숫자입니다 다른 숫자를 입력해주세요!";
+        return;
+    }
+    chances -- ;
+    chanceArea.textContent = `남은 기회: ${chances}번`;
+    console.log("chance",chances);
+
     if(userValue < computerNum) {
-        resultArea.textContent = "🔼 Up!!!"
+        resultArea.textContent = "🔼 Up!!!";
         
     }else if(userValue > computerNum) {
-        resultArea.textContent = "🔽 Down!!!"
+        resultArea.textContent = "🔽 Down!!!";
         
     }else {
-        resultArea.textContent = "🎉 축하해요! 맞추셨습니다~!"
+        resultArea.textContent = "🎉 축하해요! 맞추셨습니다~!";
         
     }
+
+    history.push(userValue);
+
+    if(chances < 1) {
+        gameOver = true;
+    }
+    if(gameOver == true) {
+        playButton.disabled = true;
+    }
  }
- pickRandomNum();
+
+ function reset() {
+    // user input창이 깨끗하게 정리되고
+    userInput.value = "";
+    //새로운 번호가 생성
+    pickRandomNum();
+
+    resultArea.textContent = "결과값이 여기 나옵니다!";
+ }
+  pickRandomNum();
